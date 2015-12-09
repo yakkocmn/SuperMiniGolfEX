fd9c26f689ed31b7ca5dbf7e0ca51980dbf7bdf1
Phacenox <omegathesigma@gmail.com>
2015-12-09 11:07:30 -0800

    Actually Have Stuff Now

--- golf.js
+++ golf.js
@@ -1,7 +1,33 @@
 
 var canvasGA = document.getElementById("gameArea");
-canvasGA.addEventListener('click', processUserInput);
-var contextGA = canvasGA.getContext("2d");
-contextGA.rect(0, 0, 1024, 576);
-contextGA.lineWidth = "10";
-contextGA.stroke();
+var ctx = canvasGA.getContext("2d");
+canvasGA.addEventListener("mousemove", setcoords, false);
+ctx.rect(0, 0, 1024, 576);
+ctx.lineWidth = "10";
+ctx.stroke();
+
+
+function setcoords(){
+  var x = event.pageX;
+  var y = event.pageY;
+ctx.rect( x, y, 10, 10);
+ctx.stroke();
+}
+
+var FPS = 30;
+setInterval(function() {
+//  update();
+  draw();
+}, 1000/FPS);
+
+function draw() {
+  ctx.rect( x, y, 10, 10);
+  ctx.stroke();
+  console.log("gfgff");
+}
+
+
+function Bual() {
+    this.image = new Image();
+    this.image.src = "buals.png";
+}
